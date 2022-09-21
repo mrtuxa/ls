9d944051f9f2c58a493f11ea50db4786895696a2
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-09-21 22:18:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"runtime"
 )
 
 func main() {
+	Module()
+}
+
+func getDirectory() string {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return path
+}
+
+func listDir() {
 	files, err := ioutil.ReadDir(getDirectory())
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	for _, f := range files {
 		colorRed := "\033[31m"
 		colorReset := "\033[0m"
@@ -21,10 +34,20 @@ func main() {
 	}
 }
 
-func getDirectory() string {
-	path, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
+func Module() {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+		listDir()
+	case "linux":
+		fmt.Println("Linux.")
+		listDir()
+	case "windows":
+		log.Fatal("\nWindows is currently not supported")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+		listDir()
 	}
-	return path
 }
